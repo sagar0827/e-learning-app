81b7d5937ce9fcb698b204ec23d0d74e21590964
sagar kushwah <kushwahsagar9685@gmail.com>
2026-05-20 02:40:28 +0530

    added authantication

--- models/User.js
+++ models/User.js
@@ -1,21 +1,39 @@
-const {Schema} = require('mongoose');
-const mongoose = require('mongoose');
+// const {Schema} = require('mongoose');
+// const mongoose = require('mongoose');
+
+// const userSchema = new mongoose.Schema({
+//   username: {
+//     type: String,
+//     required: true,
+//     unique: true
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true
+//   },
+//   password: {
+//     type: String,
+//     required: true
+//   }
+// });
+
+// module.exports = mongoose.model('User', userSchema);
+
+const mongoose = require("mongoose");
+
+const  passportLocalMongoose  = require("passport-local-mongoose").default;
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
 });
 
-module.exports = mongoose.model('User', userSchema);
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User",userSchema );
